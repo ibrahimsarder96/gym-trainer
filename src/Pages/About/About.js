2b89c5d2d6c2b926f3029b5285ed3cfc121c5dff
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2022-04-18 16:34:55 +0600

    about added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './About.css'
+import photo1 from '../../images/me/photo-1.jpg'
 
 const About = () => {
   return (
-    <div>
-      <h1>This About page</h1>
+      <div className="container">
+        <img className='photo mt-2' src={photo1} alt="" />
+        <h2 className='mt-3'>Md.Ibrahim Sarder</h2>
+        <p>My only goal for the next four months is to prepare for a good quality web development.I will do an internship in a good company.My goal is to be a fullstack and problem solver.This is my big mission</p>
     </div>
   );
 };
